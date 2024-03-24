13ce3c0c7376c9634cd793bf98c8b55bde245ffb
SemihMT <SemihMT@users.noreply.github.com>
2024-03-24 21:44:17 +0100

    Changed how text is rendered to suppoort UTF-8 & newline characters

--- Minigin/Text.cpp
+++ Minigin/Text.cpp
@@ -39,7 +39,7 @@ void Text::Update()
 	if (m_needsUpdate)
 	{
 		constexpr SDL_Color color = { 255,255,255,255 }; // only white text is supported now
-		const auto surf = TTF_RenderText_Blended(m_font->GetFont(), m_text.c_str(), color);
+		const auto surf = TTF_RenderUTF8_Blended_Wrapped(m_font->GetFont(), m_text.c_str(), color,0);
 		if (surf == nullptr)
 		{
 			throw std::runtime_error(std::string("Render text failed: ") + SDL_GetError());
